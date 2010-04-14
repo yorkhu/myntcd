1d20c41fa2e6642189d6d9ef99dae6f5fe9d5fb8
York <york@freemail.hu>
2010-04-14 19:55:14 +0200

    New RRD tool script.

---
+++ bin/rrdrest.php
@@ -0,0 +1,28 @@
+#! /usr/bin/php
+<?php
+$config['rrd_dir'] = '/usr/local/myntcd/rrd/';
+$config['rrd_dump_dir'] = '/usr/local/myntcd/xml/';
+$config['rrd_cmd'] = '/usr/bin/rrdtool';
+
+function xml2rrd($file) {
+    return str_replace('.xml', '', $file);
+}
+
+/** Konyvtar emgnyitasa */
+if ($dir = @opendir($config['rrd_dump_dir'])) {
+        /** Beolvassa a konyvtarban levo fileokat.*/
+        $files = array();
+        $i = 0;
+        while ( ($file = readdir($dir)) ) {
+                if ($file != '.' && $file != '..' && !is_dir($config['rrd_dump_dir'].'/'.xml2rrd($file))) {
+                        if (!is_file($config['rrd_dir'].'/'.xml2rrd($file))) {
+                                $i++;
+                                echo $i.' - '.$config['rrd_cmd'].' restore '.$config['rrd_dump_dir'].$file.' '.$config['rrd_dir'].xml2rrd($file)."\n";
+                                exec($config['rrd_cmd'].' restore '.$config['rrd_dump_dir'].$file.' '.$config['rrd_dir'].xml2rrd($file), $error );
+                        }
+                }
+        }
+}
+
+?>
+
---
+++ bin/rrdump.php
@@ -0,0 +1,23 @@
+#! /usr/bin/php
+<?php
+$config['rrd_dir'] = '/usr/local/myntcd/myntcd/rrd/';
+$config['rrd_dump_dir'] = '/usr/local/myntcd/myntcd/rrd-dump/';
+$config['rrd_cmd'] = '/usr/bin/rrdtool';
+
+/** Konyvtar emgnyitasa */
+if ($dir = @opendir($config['rrd_dir'])) {
+        /** Beolvassa a konyvtarban levo fileokat.*/
+        $files = array();
+        $i = 0;
+        while ( ($file = readdir($dir)) ) {
+                if ($file != '.' && $file != '..' && !is_dir($config['rrd_dir'].'/'.$file)) {
+                        if (!is_file($config['rrd_dump_dir'].'/'.$file.'.xml')) {
+                                $i++;
+                                echo $i.' - '.$config['rrd_cmd'].' dump '.$config['rrd_dir'].$file.' > '.$config['rrd_dump_dir'].$file.'.xml'."\n";
+                                exec($config['rrd_cmd'].' dump '.$config['rrd_dir'].$file.' > '.$config['rrd_dump_dir'].$file.'.xml', $error );
+                        }
+                }
+        }
+}
+
+?>
