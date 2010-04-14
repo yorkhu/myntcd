54653751b21f685849c80e7b50271a49381f998e
York <york@freemail.hu>
2010-04-14 19:30:15 +0200

    New SQL structure.

--- docs/mysql.sql
+++ docs/mysql.sql
@@ -2,52 +2,56 @@ CREATE DATABASE traffic;
 
 use traffic;
 
-CREATE TABLE rrd_now (
-  id bigint(20) unsigned NOT NULL auto_increment,
-  ip varchar(15) default NULL,
-  date datetime NOT NULL default '0000-00-00 00:00:00',
-  type int(3) NOT NULL default '0',
-  in_count int(10) unsigned NOT NULL default '0',
-  out_count int(10) unsigned NOT NULL default '0',
-  in_byte bigint(20) unsigned NOT NULL default '0',
-  out_byte bigint(20) unsigned NOT NULL default '0',
-  PRIMARY KEY  (id)
-) TYPE=MyISAM AUTO_INCREMENT=0 ;
+DROP TABLE IF EXISTS `now`;
+CREATE TABLE `now` (
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `ip` varchar(15) default NULL,
+  `date` datetime NOT NULL default '0000-00-00 00:00:00',
+  `type` int(3) NOT NULL default '0',
+  `in_packet` int(10) unsigned NOT NULL default '0',
+  `out_packet` int(10) unsigned NOT NULL default '0',
+  `in_byte` bigint(20) unsigned NOT NULL default '0',
+  `out_byte` bigint(20) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 ;
 
-CREATE TABLE rrd_hourly (
-  id bigint(20) unsigned NOT NULL auto_increment,
-  ip varchar(15) default NULL,
-  date datetime NOT NULL default '0000-00-00 00:00:00',
-  type int(3) NOT NULL default '0',
-  in_count int(10) unsigned NOT NULL default '0',
-  out_count int(10) unsigned NOT NULL default '0',
-  in_byte bigint(20) unsigned NOT NULL default '0',
-  out_byte bigint(20) unsigned NOT NULL default '0',
-  PRIMARY KEY  (id)
-) TYPE=MyISAM AUTO_INCREMENT=0 ;
+-- --------------------------------------------------------
 
-CREATE TABLE rrd_daily (
-  id bigint(20) unsigned NOT NULL auto_increment,
-  ip varchar(15) default NULL,
-  date datetime NOT NULL default '0000-00-00 00:00:00',
-  type int(3) NOT NULL default '0',
-  in_count int(10) unsigned NOT NULL default '0',
-  out_count int(10) unsigned NOT NULL default '0',
-  in_byte bigint(20) unsigned NOT NULL default '0',
-  out_byte bigint(20) unsigned NOT NULL default '0',
-  PRIMARY KEY  (id)
-) TYPE=MyISAM AUTO_INCREMENT=0 ;
+DROP TABLE IF EXISTS `daily`;
+CREATE TABLE `daily` (
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `ip` varchar(15) default NULL,
+  `date` date NOT NULL default '0000-00-00',
+  `type` int(3) NOT NULL default '0',
+  `in_packet` int(10) unsigned NOT NULL default '0',
+  `out_packet` int(10) unsigned NOT NULL default '0',
+  `in_byte` bigint(20) unsigned NOT NULL default '0',
+  `out_byte` bigint(20) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 ;
 
-CREATE TABLE blacklist (
-    ip varchar(15) NOT NULL default '',
-    date date NOT NULL default '0000-00-00',
-    PRIMARY KEY  (ip)
-) TYPE=MyISAM;
-      
+-- --------------------------------------------------------
 
-CREATE TABLE whitelist (
-  ip varchar(15) NOT NULL default '',
-  date date NOT NULL default '0000-00-00',
-  PRIMARY KEY  (ip)
-) TYPE=MyISAM;
+DROP TABLE IF EXISTS `filter`;
+CREATE TABLE `filter` (
+  `ip` varchar(15) NOT NULL default '',
+  `blacklist` smallint(1) unsigned NOT NULL default '0',
+  `date` date NOT NULL default '0000-00-00',
+  `comment` varchar(255) NOT NULL,
+  PRIMARY KEY  (`ip`)
+) ENGINE=MyISAM;
 
+-- --------------------------------------------------------
+
+DROP TABLE IF EXISTS `tmp`;
+CREATE TABLE `tmp` (
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `ip` varchar(15) default NULL,
+  `date` datetime NOT NULL default '0000-00-00 00:00:00',
+  `type` int(3) NOT NULL default '0',
+  `in_packet` int(10) unsigned NOT NULL default '0',
+  `out_packet` int(10) unsigned NOT NULL default '0',
+  `in_byte` bigint(20) unsigned NOT NULL default '0',
+  `out_byte` bigint(20) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=0;
