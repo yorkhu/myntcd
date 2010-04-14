bc354eab44ef37d3fea65adb4c0794f2eb6497cb
York <york@freemail.hu>
2010-04-14 19:29:28 +0200

    Drupal 4.7.x web support.

--- web/ip_stat.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ((count($_GET) == 1)) {
-	$ip = htmlspecialchars($_GET['ip']);
-	$site = 'ip_stat.php?ip='.$ip;
-	require_once('stat.php');
-}
-?>
--- web/mystat.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ((count($_GET) == 0)) {
-	$ip = $_SERVER['REMOTE_ADDR'];
-	$site='mystat.php';
-	require_once('stat.php');
-}
-?>
-
